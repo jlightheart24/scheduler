b80f4c77056f4f1f29315a2214ed187b031a64be
Jonny Lightheart <jonnylightheart@gmail.com>
2020-10-30 19:23:38 -0600

    added script

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,3 +1,27 @@
-const moment = require('moment');
+var timeBlockNotes = {};
 
-console.log(moment().format('MMMM Do YYYY, h:mm:ss a'));
+//Display Current Time
+var displayCurrentTime = function() {
+    var currentTime = moment().format('MMMM Do YYYY, h:mm:ss a');
+    $('#currentDay').append(currentTime);
+};
+
+//Save text in time blocks
+var saveNotes = function() {
+    localStorage.setItem('notes', JSON.stringify(timeBlockNotes));
+};
+
+
+// var loadNotes = function() {
+//     if(!timeBlockNotes) {
+//         timeBlockNotes = {
+//             innerText: [],
+//             timeBlock: [],
+//         }
+//     }
+// };
+
+displayCurrentTime();
+
+
+
